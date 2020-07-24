bf567b5d2ad2af129d2aed8328742fcbb89cae63
nopnops <18600878681@163.com>
2020-07-24 17:44:44 +0800

    xiaochegxu0.0.1

---
+++ miniprogram/config/navRouter.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    "pagePath": "跳转路径",
+    "text": "首页",
+    "iconPath": "",
+    "selectedIconPath": "",
+    "auth": 0
+  },
+  {
+    "pagePath": "跳转路径",
+    "text": "singto",
+    "iconPath": "",
+    "selectedIconPath": "",
+    "auth": 0
+  },
+  {
+    "pagePath": "跳转路径",
+    "text": "our",
+    "iconPath": "",
+    "selectedIconPath": "",
+    "auth": 0
+  },
+  {
+    "pagePath": "跳转路径",
+    "text": "krist",
+    "iconPath": "",
+    "selectedIconPath": "",
+    "auth": 0
+  },
+  {
+    "pagePath": "跳转路径",
+    "text": "peraya",
+    "iconPath": "",
+    "selectedIconPath": "",
+    "auth": 0
+  },
+]
